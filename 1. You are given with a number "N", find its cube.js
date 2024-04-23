733285c0087d58f491dc563486023a81465b6193
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-04-23 21:38:28 +0530

    You are given with a number "N", find its cube.js
    Input Description:
    A positive integer is provided.
    
    Output Description:
    Find the cube of the number.
    
    Sample Input :
    2
    Sample Output :
    8

---
+++ 1. You are given with a number "N", find its cube.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+var a =parseInt(userInput[0]);
+var b = a*a*a;
+console.log(b);
+
+  //end-here
+});
