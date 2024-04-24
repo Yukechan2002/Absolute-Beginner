50cbe83d3a6ac5a2a8a3d748fd6a626b683fe252
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-04-24 20:41:56 +0530

    4. You are provided with the radius of a circle "A". Find the length of its circumference.js
    Note: In case the output is coming in decimal, roundoff to 2nd decimal place. In case the input is a negative number, print "Error".
    
    Input Description:
    The Radius of a circle is provided as the input of the program.
    
    Output Description:
    Calculate and print the Circumference of the circle corresponding to the input radius up to two decimal places.
    
    Sample Input :
    2
    Sample Output :
    12.57

---
+++ 4. You are provided with the radius of a circle "A". Find the length of its circumference.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+var input = parseFloat(userInput[0]);
+var output = (2*22*input)/7
+console.log(output.toFixed(2));
+
+  //end-here
+});
