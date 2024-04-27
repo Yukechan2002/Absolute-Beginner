19cbf013baf97888686144f08d31cdb888412dea
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-04-27 21:35:24 +0530

    Create 18. Let "A"  be a string. Remove all the whitespaces and find it's length.js
    Input Description:
    A string is provided as an input
    
    Output Description:
    Remove all the whitespaces and then print the length of the remaining string.
    
    Sample Input :
    Lorem Ipsum
    Sample Output :
    10

---
+++ 18. Let "A"  be a string. Remove all the whitespaces and find it's length.js
@@ -0,0 +1,27 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+var input= userInput[0];
+var count=0;
+for(var i = 0;i<input.length;i++){
+    if(input[i]!=" "){
+        count++;
+    }
+}
+console.log(count);
+  //end-here
+});
