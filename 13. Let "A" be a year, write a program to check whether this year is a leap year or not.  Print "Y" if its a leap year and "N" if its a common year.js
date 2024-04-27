c4396dbb67f527b7143ffde3bac9198356391ebd
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-04-27 21:30:30 +0530

    Create 13. Let "A" be a year, write a program to check whether this year is a leap year or not.  Print "Y" if its a leap year and "N" if its a common year.js
    Print "Y" if its a leap year and "N" if its a common year.
    
    Input Description:
    A Year is the input in the form of a positive integer.
    
    Output Description:
    Print "Y" if its a leap year and "N" if its a common year.
    
    Sample Input :
    2020
    Sample Output :
    Y

---
+++ 13. Let "A" be a year, write a program to check whether this year is a leap year or not.  Print "Y" if its a leap year and "N" if its a common year.js
@@ -0,0 +1,25 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+var year = parseInt(userInput[0]);
+ if ((year%4 ==0 && year % 100!=0) || (year % 400 == 0)){
+     console.log("Y");
+ }
+     else
+     console.log("N");
+
+  //end-here
+});
